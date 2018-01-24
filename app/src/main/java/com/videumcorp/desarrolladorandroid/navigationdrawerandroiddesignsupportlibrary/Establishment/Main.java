1d63a6f201a213e2cfa7f6ed35618b0ec4e4d901
Roberto <cloroxxz@hotmail.com>
2018-01-24 16:46:06 -0300

    Cambios hecho en practica

--- app/src/main/java/com/videumcorp/desarrolladorandroid/navigationdrawerandroiddesignsupportlibrary/Establishment/Main.java
+++ app/src/main/java/com/videumcorp/desarrolladorandroid/navigationdrawerandroiddesignsupportlibrary/Establishment/Main.java
@@ -11,6 +11,7 @@ import android.location.Geocoder;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.Telephony;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -336,6 +337,14 @@ public class Main extends AppCompatActivity {
 
     public void OnMapReady(GoogleMap googleMap)
     {
+    }
+
+    public void zoom_map(View view){
+        Intent intent = new Intent(this,ZoomMap.class);
+        intent.putExtra("name", Company);
+        intent.putExtra("email", Email);
+        intent.putExtra("address", Address);
+        startActivity(intent);
     }
 
     @Override
